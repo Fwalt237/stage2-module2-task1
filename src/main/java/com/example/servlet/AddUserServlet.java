59b084b2debd12e5d8b4bb7d1f1671321775804d
Fwalt237 <patrickkamwa17@gmail.com>
2025-06-05 08:54:19 +0100

    first

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,42 @@
 package com.example.servlet;
 
-public class AddUserServlet {
+
+import com.example.User;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+import static com.example.Warehouse.getInstance;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
     //write your code here!
+    public AddUserServlet() {
+        super();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/add.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        User user = new User(firstName, lastName);
+        getInstance().addUser(user);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/add.jsp");
+        dispatcher.forward(request, response);
+    }
+
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,25 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
     //write your code here!
+    public GetUsersServlet() {
+        super();
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/users.jsp");
+        dispatcher.forward(request, response);
+    }
+
 }
